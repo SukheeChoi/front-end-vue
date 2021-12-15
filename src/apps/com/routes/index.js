9e904ba00072855704f34e450f8bdb0261ece2d0
Lee MyungGyu <myunggyue.lee@osstem.com>
2021-12-15 14:42:11 +0900

    sample 추가

--- src/apps/com/routes/index.js
+++ src/apps/com/routes/index.js
@@ -1,1 +1,14 @@
-export default [];
+export default [
+  {
+    path: '/com',
+    name: 'com',
+    component: () => import('@/views/AppMain'),
+    children: [
+      {
+        path: 'BatchMgmt',
+        name: 'COMBT001',
+        component: () => import('@/apps/com/views/TheBatchMgmt'),
+      },
+    ],
+  },
+];
