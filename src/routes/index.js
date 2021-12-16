424112ed2b06ff57075ad3bbaf1e19bf57038e6a
kst <kst@osstem.com>
2021-12-16 11:16:19 +0900

    feat : 로그인 관련 라우터 가드 기능 추가 ( 현재는 주석)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,62 +1,79 @@
+import { nextTick } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
+import store from '../store';
 
-const routes = [
-  {
-    path: '/',
-    name: 'main',
-    component: () => import('@/views/AppMain'),
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: () => import('@/views/AppLogin'),
-  },
-  {
-    path: '/:pathMatch(.*)*',
-    component: () => import('@/views/AppNotFound'),
-  },
-  {
-    path: '/',
-    name: 'mainLayout',
-    component: () => import('../views/layout/mainLayout.vue'),
-  },
-  {
-    path: '/oss-login',
-    name: 'login',
-    component: () => import('../views/login/OssLogin.vue'),
-  },
-  // {
-  //   path: "/part-timer-login",
-  //   name: "partTimerLogin",
-  //   component: () => import ("../views/login/PartTimerLogin.vue")
-  // },
-  // {
-  //   path: "/sup-login",
-  //   name: "supLogin",
-  //   component: () => import ("../views/login/SupLogin.vue")
-  // },
-  {
-    path: '/admin',
-    name: 'adminMain',
-    component: () => import('../views/admin/Main.vue'),
-  },
+const routes = [{
+        path: '/',
+        name: 'main',
+        component: () =>
+            import ('@/views/AppMain'),
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: () =>
+            import ('@/views/AppLogin'),
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        component: () =>
+            import ('@/views/AppNotFound'),
+    },
+    {
+        path: '/',
+        name: 'mainLayout',
+        component: () =>
+            import ('../views/layout/mainLayout.vue'),
+    },
+    {
+        path: '/oss-login',
+        name: 'ossLogin',
+        component: () =>
+            import ('../views/login/OssLogin.vue'),
+    },
+    // {
+    //   path: "/part-timer-login",
+    //   name: "partTimerLogin",
+    //   component: () => import ("../views/login/PartTimerLogin.vue")
+    // },
+    // {
+    //   path: "/sup-login",
+    //   name: "supLogin",
+    //   component: () => import ("../views/login/SupLogin.vue")
+    // },
+    {
+        path: '/admin',
+        name: 'adminMain',
+        component: () =>
+            import ('../views/admin/Main.vue'),
+    },
 ];
 
 const apps = (process.env.VUE_APP_APPS || '').split(',');
 
 for (const app of apps) {
-  let module;
-  try {
-    module = require(`@/apps/${app.trim().toLowerCase()}/routes`);
-    if (module && module.default && Array.isArray(module.default)) {
-      routes.push(...module.default);
+    let module;
+    try {
+        module = require(`@/apps/${app.trim().toLowerCase()}/routes`);
+        if (module && module.default && Array.isArray(module.default)) {
+            routes.push(...module.default);
+        }
+    } catch (error) {
+        console.error(error);
     }
-  } catch (error) {
-    console.error(error);
-  }
 }
 
-export default createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
+/*
+router.beforeEach((to, from, next) => {
+    if (to.name !== 'ossLogin' && store.state.login.accessToken == '') {
+        next('/oss-login');
+    }
+    next();
+
+});
+*/
+export default router;
