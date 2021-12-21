c7dda3bf8710c77386406e80c2bca044e4e6d0b7
kanghun <kanghun@osstem.com>
2021-12-21 17:08:26 +0900

    데이터표준 - 표준단어 화면 수정, 체크박스 컴포넌트 수정

--- src/apps/com/components/wijmo/grid/OwCheckColumn.vue
+++ src/apps/com/components/wijmo/grid/OwCheckColumn.vue
@@ -1,61 +1,79 @@
 <template>
-    <template>
-        <wj-flex-grid-cell-template cellType="ColumnHeader" v-slot="cell">
-            <!-- <input type="checkbox" v-model="isAllChecked" @click="allCheck(cell.row.collectionView, $event)" /> -->
-            <input type="checkbox" :checked="isAllChecked" @click="allCheck(cell.row.collectionView.items, $event)" />
+    <template :initialized="initialized">
+        <wj-flex-grid-cell-template cellType="ColumnHeader" v-if="all">
+            <input type="checkbox" :checked="isAllChecked" @input="onAllCheck($event.target)" />
         </wj-flex-grid-cell-template>
         <wj-flex-grid-cell-template cellType="Cell" v-slot="cell">
-            <!-- <input type="checkbox" :id="cell.row.index + 1" name="checkCol" @click="clickFunc(cell.row.dataItem, $event)" /> -->
-            <input type="checkbox" name="checkCol" @click="clickFunc(cell.row.dataItem, $event)" />
+            <input type="checkbox" :checked="isChecked(cell)" @input="onCheck(cell, $event.target)" />
         </wj-flex-grid-cell-template>
     </template>
 </template>
 <script>
 import { WjFlexGridColumn } from '@grapecity/wijmo.vue2.grid';
-import { nextTick } from 'vue';
+import { reactive, computed } from 'vue';
 
 export default {
     name: 'OwCheckColumn',
     extends: WjFlexGridColumn,
-    computed: {
-        isAllChecked() {
-            return this.checkedValues.length === this.cellMaxLength;
+    props: {
+        all: {
+            type: Boolean,
+            default: true,
+        },
+        modelValue: {
+            type: Array,
+            default: () => {
+                return [];
+            },
         },
     },
-    data: function () {
-        return {
-            cellMaxLength: 0,
-            checkedValues: [],
-        };
-    },
-    methods: {
-        allCheck(data, event) {
-            let checkCol = document.getElementsByName('checkCol');
-            let isChecked = event.target.checked;
+    setup(props, { emit }) {
+        const { value } = computed({
+            get: () => props.modelValue,
+            set: (value) => emit('update:modelValue', value),
+        });
 
-            checkCol.forEach((el) => (el.checked = isChecked));
-            this.checkedValues.splice(0);
-            if (isChecked) {
-                this.checkedValues.push(...data);
+        let column = null;
+
+        const initialized = (col) => {
+            column = reactive(col);
+        };
+
+        const isAllChecked = computed(() => {
+            const { collectionView } = column;
+            return (collectionView && collectionView.itemCount) === value.length;
+        });
+
+        const isChecked = ({ item }) => {
+            return value.indexOf(item) > -1;
+        };
+
+        const onAllCheck = ({ checked }) => {
+            const { collectionView } = column;
+
+            value.splice(0);
+
+            if (checked) {
+                value.push(...collectionView.items);
             }
-            this.$emit('checkedData', this.checkedValues);
-        },
-        clickFunc(data, event) {
-            const isChecked = event.target.checked;
-            if (isChecked) {
-                this.checkedValues.push(data);
+        };
+
+        const onCheck = ({ item }, { checked }) => {
+            const at = value.indexOf(item);
+            if (checked) {
+                value.push(item);
             } else {
-                const index = this.checkedValues.indexOf(data);
-                this.checkedValues.splice(index, 1);
+                value.splice(at, 1);
             }
-            this.$emit('checkedData', this.checkedValues);
-        },
-    },
-    mounted() {
-        // this.cellMaxLength = document.getElementsByName('checkCol').length;
-        //nextTick().then(() => {});
-        console.log(this.$refs);
-        console.log('최대 길이는 ?? ', this.cellMaxLength);
+        };
+
+        return {
+            initialized,
+            isAllChecked,
+            isChecked,
+            onAllCheck,
+            onCheck,
+        };
     },
 };
 </script>
