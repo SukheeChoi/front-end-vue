775c6ed3e998e67a18e2884d25b062b9ea9c06ae
Lee MyungGyu <myunggyue.lee@osstem.com>
2021-12-27 14:34:59 +0900

    confirm 추가

---
+++ src/components/common/OwConfirm.vue
@@ -0,0 +1,40 @@
+<template>
+  <ow-modal ref="root">
+    {{ message }}
+  </ow-modal>
+</template>
+<script>
+import OwModal from '@/components/common/OwModal';
+import { ref } from 'vue';
+export default {
+  name: 'OwConfirm',
+  components: {
+    OwModal,
+  },
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {
+    const root = ref(null);
+
+    const open = () => root.value.open();
+    const close = () => root.value.close();
+
+    return {
+      root,
+      open,
+      close,
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+::v-deep {
+  .modal-header {
+    height: 0px;
+  }
+}
+</style>
