322a4cc671b58cbf57503f60ecf8c5d48fea9d3a
Lee MyungGyu <myunggyue.lee@osstem.com>
2021-12-28 11:55:57 +0900

    ow-select label 추가

--- src/components/common/OwSelect.vue
+++ src/components/common/OwSelect.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="ow-select">
+  <div class="ow-select" ref="root">
     <select v-model="selectedValue">
       <slot></slot>
       <option v-for="{ name, value } in items" :key="value" :value="value">
@@ -9,11 +9,12 @@
   </div>
 </template>
 <script>
-import { computed } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 
 export default {
   name: 'OwSelect',
   props: {
+    label: String,
     items: {
       type: Array,
       default: () => {
@@ -23,6 +24,12 @@ export default {
     modelValue: [String, Number],
   },
   setup(props, { emit }) {
+    const root = ref(null);
+
+    const expando = computed(() => {
+      return 'ow-input-' + ('' + Math.random()).replace(/\D/g, '');
+    });
+
     const isNumber = computed(() => {
       return (
         props.modelValue instanceof Number ||
@@ -36,7 +43,18 @@ export default {
         emit('update:modelValue', isNumber.value ? +value : value),
     });
 
+    onMounted(() => {
+      if (props.label) {
+        const label = document.createElement('label');
+        label.classList.add('t');
+        label.setAttribute('for', expando.value);
+        label.textContent = props.label;
+        root.value.parentNode.insertBefore(label, root.value);
+      }
+    });
+
     return {
+      root,
       selectedValue,
     };
   },
