1d6ea32610340d09856f52e7a488da74d72b2422
BumJo Kim <BumJo Kim@OST25070511N>
2021-12-29 13:47:26 +0900

    Login Vue/Route 변경

--- src/views/AppMain.vue
+++ src/views/AppMain.vue
@@ -1,110 +1,123 @@
 <template>
-  <ow-spinner :loading="false"></ow-spinner>
-  <div id="main">
-    <app-header></app-header>
-    <div id="wrap" class="wrap">
-      <div class="content">
-        <app-nav class="content_tab"></app-nav>
-        <main class="content_body">
-          <app-section
-            class="ow-container col-2-set"
-            :class="{ 'main__article--show-left': hasLeft }"
-            style="--size-1: 0; --gap-container: 0"
-            ref="container"
-          >
-            <!-- Left -->
-            <div class="ow-content" :class="{ 'pr-2': hasLeft }">
-              <app-article
-                class="ow-flex-wrap dir-col size-full"
-                v-show="left.show"
-              >
-                <div
-                  class="item size-fix"
-                  style="--size: 70%; --bg: transparent"
-                >
-                  <the-action-plan></the-action-plan>
+  <div class="loginWrap">
+    <div class="owLiginWarp">
+      <div class="owLogin">
+        <div class="mainLogin"><i>Oway</i></div>
+        <div class="lginGroup">
+          <label for="selcectCompany">회사선택</label>
+          <select id="" v-model="companyType">
+            <option value="" disabled selected hidden>회사를 선택하세요</option>
+            <option value="osstem">오스템임플란트(주)</option>
+            <option value="osstemPama">오스템파마</option>
+          </select>
+          <label for="inputLoginId">사원번호</label>
+          <input
+            type="text"
+            id="inputLoginId"
+            v-model="loginId"
+            ref="userId"
+            placeholder="사원번호"
+            @keyup.enter="onClickLogin"
+          />
+          <label for="inputPassword">비밀번호</label>
+          <input
+            type="password"
+            id="inputPassword"
+            v-model="password"
+            ref="password"
+            placeholder="비밀번호"
+            @keyup.enter="onClickLogin"
+          />
+
+          <label>
+            <input type="checkbox" class="k-checkbox mb-05" v-model="checkSaveUserId" />
+            <span class="status-title ml-10">사원번호 저장</span>
+          </label>
+          <!-- <label>로그인 구분</label>
+          <fieldset id="radio-group2" class="radio-group login">
+              <input type="radio" class="radio-type" id="radio-oss" name="radio-group2" checked><label for="radio-oss" Style="width:30%;">정직원</label>
+              <input type="radio" class="radio-type" id="radio-sup" name="radio-group2"><label for="radio-sup" Style="width:30%;">협력사</label>
+              <input type="radio" class="radio-type" id="radio-part-timer" name="radio-group2"><label for="radio-part-timer">도급직원</label>
+          </fieldset> -->
+          <dl class="radio-title-wrap">
+            <dt>로그인 구분</dt>
+            <dd>
+              <div class="radio-group">
+                <div class="ow-radio">
+                  <input type="radio" id="ow-radio7" name="radio-group-3" />
+                  <label for="ow-radio7">정직원</label>
                 </div>
-                <div class="item">
-                  <the-approval></the-approval>
+
+                <div class="ow-radio">
+                  <input type="radio" id="ow-radio9" name="radio-group-3" />
+                  <label for="ow-radio9">협력사</label>
                 </div>
-              </app-article>
-            </div>
-            <!-- Right -->
-            <div class="ow-content" :class="{ 'pl-2': hasLeft }">
-              <app-article class="ow-flex-wrap size-full">
-                <div class="item">
-                  <router-view></router-view>
+
+                <div class="ow-radio">
+                  <input type="radio" id="ow-radio10" name="radio-group-3" />
+                  <label for="ow-radio10">도급직원</label>
                 </div>
-              </app-article>
-            </div>
-          </app-section>
-        </main>
+              </div>
+            </dd>
+          </dl>
+
+          <button @click="getToken" class="btn-login">로그인</button>
+          <p class="adressMain">
+            로그인 관련 문의는 담당 관리자에게 문의하세요.<br />
+            담당자 연락처: 02-1234-5678 / admin@osstem.com
+          </p>
+          <p class="copyright">ⓒOsstem Implant Co.,LTD. All right reserved.</p>
+        </div>
       </div>
     </div>
   </div>
-  <app-footer></app-footer>
-  <app-aside ref="aside"></app-aside>
 </template>
+
 <script>
-import AppHeader from '@/components/AppHeader';
-import AppNav from '@/components/AppNav';
-import AppSection from '@/components/AppSection';
-import AppArticle from '@/components/AppArticle';
-import AppFooter from '@/components/AppFooter';
-import AppAside from '@/components/AppAside';
-
-import TheActionPlan from '@@/tsk/components/TheActionPlan';
-import TheApproval from '@@/eap/components/TheApproval';
-import { computed } from 'vue';
-
+import router from '@/routes';
+import axios from 'axios';
+import login from '@/api/login.js';
+import restApi from '@/api/restApi.js';
 export default {
-  components: {
-    AppHeader,
-    AppNav,
-    AppSection,
-    AppArticle,
-    AppFooter,
-    AppAside,
-    TheActionPlan,
-    TheApproval,
-  },
-  props: {
-    nav: {
-      type: Object,
-      default: function () {
-        return {};
-      },
-    },
-    left: {
-      type: Object,
-      default: function () {
-        return {
-          show: false,
-        };
-      },
-    },
-    right: {
-      type: Object,
-      default: function () {
-        return {};
-      },
-    },
-  },
-  setup(props, context) {
-    console.log('setup', props, context);
-
-    const hasLeft = computed(() => props.left.show);
-
+  name: 'OssLogin',
+  data() {
     return {
-      hasLeft,
+      companyType: 'osstem',
+      loginId: '',
+      password: '',
+      checkSaveUserId: false,
     };
+  },
+  methods: {
+    async getToken() {
+      const tokenData = await login.requestLogin('/com/login/do-login', this.loginId, this.password);
+      if (tokenData.data.data !== null) {
+        const accessToken = tokenData.data.data;
+        //localStorage.setItem("token", accessToken);
+        //console.log(this.$store);
+        this.$store.commit('setToken', accessToken);
+        //console.log(test);
+        return await this.getUserInfo();
+      }
+    },
+    async getUserInfo() {
+      const userData = await login.getUserInfo('/com/login/user-info', this.loginId);
+      if (userData.data.data !== null) {
+        const userInfo = userData.data.data;
+        //localStorage.setItem("userInfo", JSON.stringify(userInfo));
+        this.$store.commit('setUserInfo', userInfo);
+        return await this.getMenus();
+        //this.$router.push("/");
+      }
+    },
+    async getMenus() {
+      const menuData = await restApi.get('/com/menu/test', this.loginId);
+      if (menuData.data !== null) {
+        const menuList = menuData.data;
+        this.$store.commit('setMenus', menuList);
+        this.$router.push('/');
+      }
+    },
   },
 };
 </script>
-<style lang="scss" scoped>
-.main__article--show-left {
-  &.ow-container {
-    --size-1: 446px !important;
-  }
-}
-</style>
