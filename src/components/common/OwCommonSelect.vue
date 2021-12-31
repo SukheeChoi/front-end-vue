2a8befc92648f3a22fe063c7e0c3cacb3b01a181
Jung Jaewon <jjwon@osstem.com>
2021-12-31 09:42:12 +0900

    공통코드 조회 콤보박스 추가

--- src/components/common/OwCommonSelect.vue
+++ src/components/common/OwCommonSelect.vue
@@ -1,47 +1,94 @@
 <template>
-  <ow-select :options="item" :all="all"></ow-select>
+  <wj-combo-box :itemsSource="items" :initialized="initialized"> </wj-combo-box>
 </template>
 <script>
-import { reactive, onMounted } from 'vue';
-
-import OwSelect from '@/components/common/OwSelect';
+import { reactive } from 'vue';
+import { CollectionView } from '@grapecity/wijmo';
+import restApi from '@/api/restApi.js';
 
 export default {
   name: 'OwCommonSelect',
-  components: {
-    OwSelect,
-  },
   props: {
-    all: {
-      type: Boolean,
-      default: true,
-    },
     codeGroup: {
       type: String,
       default: '',
     },
-    modelValue: [String, Number],
+    options: String,
+    linked: String,
+    filter: String,
   },
   setup(props) {
     const { codeGroup } = reactive(props);
 
-    let item = reactive([]);
+    let items = reactive(new CollectionView([]));
+    let reqData = null;
 
-    if (codeGroup) {
-      item.push(
-        ...[
-          { name: '공통', value: 1 },
-          { name: '물류', value: 2 },
-        ]
-      );
+    const uri = '/com/';
+    const id = '';
+
+    const initialized = (option) => {
+      if (!option.displayMemberPath) {
+        option.displayMemberPath = 'name';
+      }
+
+      if (!option.selectedValuePath) {
+        option.selectedValuePath = 'code';
+      }
+    };
+
+    //getlist test
+    function getList(codeGroup) {
+      if (codeGroup == 'system') {
+        reqData = [
+          { code: 0, name: 'CMG' },
+          { code: 1, name: 'PAL' },
+          { code: 2, name: 'SEI' },
+          { code: 3, name: 'SAM' },
+        ];
+      } else if (codeGroup == 'biz') {
+        reqData = [
+          { biz: 0, code: 0, name: 'CIF' },
+          { biz: 1, code: 0, name: 'COM' },
+          { biz: 2, code: 0, name: 'OXF' },
+          { biz: 3, code: 0, name: 'EAP' },
+          { biz: 4, code: 0, name: 'TSK' },
+          { biz: 5, code: 1, name: 'LIF' },
+          { biz: 5, code: 1, name: 'LCM' },
+          { biz: 5, code: 1, name: 'LOG' },
+          { biz: 5, code: 1, name: 'PAT' },
+          { biz: 5, code: 1, name: 'MAL' },
+          { biz: 5, code: 1, name: 'MEM' },
+          { biz: 6, code: 2, name: 'EIF' },
+          { biz: 7, code: 2, name: 'SVC' },
+          { biz: 8, code: 2, name: 'OIC' },
+          { biz: 8, code: 2, name: 'INT' },
+          { biz: 8, code: 2, name: 'JOB' },
+          { biz: 9, code: 3, name: 'SIF' },
+          { biz: 10, code: 3, name: 'SAL' },
+        ];
+      }
     }
 
-    onMounted(() => {
-      console.log('mounted 완료!!');
-    });
+    // const getList = (codeGroup) => {
+    //   let reqData = restApi.getList(uri, Object.assign(codeGroup), id);
+
+    //   if (reqData && reqData.data.data.length > 0) {
+    //     items.clear();
+    //     items = reqData.data.data;
+    //   }
+    // };
+
+    if (codeGroup) {
+      getList(codeGroup);
+    }
+
+    if (reqData && reqData.length > 0) {
+      items.sourceCollection = reqData;
+    }
 
     return {
-      item,
+      initialized,
+      items,
     };
   },
 };
