7806c34b10ae1c4694d3d9ee1c93eb651f4ff7a7
kst <kst@osstem.com>
2022-01-05 17:58:36 +0900

    fix : restApi 메소드 수정

--- src/api/restApi.js
+++ src/api/restApi.js
@@ -9,43 +9,43 @@ const RestApi = {
     return http.get(url + '/get' + id + 'List', { params });
   },
 
-  add(url, params = {}, id = '', data = null) {
+  add(url, data, id = '', params = {}) {
     return http.post(url + '/add' + id, data, { params });
   },
 
-  addList(url, params = {}, id = '', data = null) {
+  addList(url, data, id = '', params = {}) {
     return http.post(url + '/add' + id + 'List', data, { params });
   },
 
-  modify(url, params = {}, id = '', data = null) {
+  modify(url, data, id = '', params = {}) {
     return http.post(url + '/modify' + id, data, { params });
   },
 
-  modifyList(url, params = {}, id = '', data = null) {
+  modifyList(url, data, id = '', params = {}) {
     return http.post(url + '/modify' + id + 'List', data, { params });
   },
 
-  remove(url, params = {}, id = '', data = null) {
+  remove(url, data, id = '', params = {}) {
     return http.post(url + '/remove' + id, data, { params });
   },
 
-  removeList(url, params = {}, id = '', data = null) {
+  removeList(url, data, id = '', params = {}) {
     return http.post(url + '/remove' + id + 'List', data, { params });
   },
 
-  save(url, params = {}, id = '', data = null) {
+  save(url, data, id = '', params = {}) {
     return http.post(url + '/save' + id, data, { params });
   },
 
-  approve(url, params = {}, id = '', data = null) {
+  approve(url, data, id = '', params = {}) {
     return http.post(url + '/approve' + id, data, { params });
   },
 
-  cancel(url, params = {}, id = '', data = null) {
+  cancel(url, data, id = '', params = {}) {
     return http.post(url + '/cancel' + id, data, { params });
   },
 
-  execute(url, params = {}, id = '', data = null) {
+  execute(url, data, id = '', params = {}) {
     return http.post(url + '/execute' + id, data, { params });
   },
 };
