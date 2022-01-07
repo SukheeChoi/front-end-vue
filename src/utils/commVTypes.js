16dc1963411498c3b6e24dbd93ef6e8a38d9bc9e
OST25070511N\osstem <online9@osstem.com>
2022-01-07 17:15:04 +0900

    Validator length 메시지 변경

--- src/utils/commVTypes.js
+++ src/utils/commVTypes.js
@@ -5,14 +5,14 @@ const telNoTest = /^\d{2,3}(\-)?\d{3,4}(\-)?\d{4}$/;
 const timeTest = /^([1-9]|1[0-9]):([0-5][0-9])(\s[a|p]m)$/i;
 const dateTest = /^(d{4})-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-9]|3[0-1])$/i;
 
-const LENGTH_MESSAGE = '{0} 길이는 {1}과 {2} 사이여야 합니다.';
+const LENGTH_MESSAGE = '{0} 길이는 {1}과 {2} 사이여야 합니다.\n현재 입력하신 문자열의 길이는 {3}입니다.';
 const NUMBER_MESSAGE = '{0} 숫자만 입력할 수 있습니다.';
 
 const ValidatorTypes = {
     lengthValidator(value, options) {
         return {
             isValid: value && value.length >= options.min && value.length <= options.max,
-            message: utils.makeMsg(options.message, LENGTH_MESSAGE, [options.name, options.min, options.max])
+            message: utils.makeMsg(options.message, LENGTH_MESSAGE, [options.name, options.min, options.max, value.length])
         };
     },
 
