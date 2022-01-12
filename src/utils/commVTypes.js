38f1a4da7edc4239cfd78ac5cd795547a2346e67
OST25070511N\osstem <online9@osstem.com>
2022-01-12 16:41:14 +0900

    i18n 적용

--- src/utils/commVTypes.js
+++ src/utils/commVTypes.js
@@ -1,6 +1,7 @@
-import utils from '@/utils/commUtils.js';
-import { useI18n } from 'vue-i18n';
+import Utils from '@/utils/commUtils.js';
+import i18n from '@/plugins/i18n';
 
+const { t } = i18n.global;
 const numberTest = /^(\-)?\d{1,15}$/;
 const telNoTest = /^\d{2,3}(\-)?\d{3,4}(\-)?\d{4}$/;
 const timeTest = /^([1-9]|1[0-9]):([0-5][0-9])(\s[a|p]m)$/i;
@@ -10,49 +11,49 @@ const ValidatorTypes = {
     lengthValidator(value, options) {
         return {
             isValid: value && value.length >= options.min && value.length <= options.max,
-            message: utils.makeMsg(options.message, useI18n('valid.length'), [options.name, options.min, options.max, value.length])
+            message: Utils.makeMsg(options.message, t('valid.length'), [options.name, options.min, options.max, value.length])
         };
     },
 
     numberValidator(value, options) {
         return {
             isValid: numberTest.test(value),
-            message: utils.makeMsg(options.message, useI18n('valid.number'), [options.name])
+            message: Utils.makeMsg(options.message, t('valid.number'), [options.name])
         };
     },
 
     rangeValidator(value, options) {
         return {
             isValid: value >= options.start && value <= options.end,
-            message: utils.makeMsg(options.message, useI18n('valid.range'), [options.name, options.start, options.end])
+            message: Utils.makeMsg(options.message, t('valid.range'), [options.name, options.start, options.end])
         };
     },
 
     presenceValidator(value, options) {
         return {
             isValid: value && value.length > 0,
-            message: utils.makeMsg(options.message, useI18n('valid.presence'), [options.name])
+            message: Utils.makeMsg(options.message, t('valid.presence'), [options.name])
         };
     },
 
     timeValidator(value, options) {
         return {
             isValid: timeTest.test(value),
-            message: utils.makeMsg(options.message, useI18n('time.presence'), [options.name])
+            message: Utils.makeMsg(options.message, t('time.presence'), [options.name])
         };
     },
 
     dateValidator(value, options) {
         return {
             isValid: dateTest.test(value),
-            message: utils.makeMsg(options.message, useI18n('date.presence'), [options.name])
+            message: Utils.makeMsg(options.message, t('date.presence'), [options.name])
         };
     },
 
     telNoValidator(value, options) {
         return {
             isValid: telNoTest.test(value),
-            message: utils.makeMsg(options.message, useI18n('telNo.presence'), [options.name])
+            message: Utils.makeMsg(options.message, t('telNo.presence'), [options.name])
         };
     }
 }
