805eff4d27fa7d066fad8c0c06b64e31f38c5498
Lee MyungGyu <myunggyue.lee@osstem.com>
2022-01-12 17:52:43 +0900

    input label 수정

--- src/components/common/OwInput.vue
+++ src/components/common/OwInput.vue
@@ -1,6 +1,6 @@
 <template>
   <template v-if="label">
-    <label class="t" :for="unique">{{ label }}</label>
+    <label class="input-label" :for="unique">{{ label }}</label>
   </template>
   <div class="ow-combobox" ref="root" v-bind="$attrs">
     <wj-combo-box
@@ -82,9 +82,13 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.t {
-  display: inline-flex;
-  flex: 0 0 auto;
+.input-label {
+  font-size: 13px;
+  font-weight: 700;
+  letter-spacing: -1.08px;
+  color: #333;
+  margin-right: 6px;
+  flex-shrink: 0;
 }
 .ow-combobox {
   .btn-search {
