10cc86bb773316928549879bcb43be36c9d5abc9
Lee MyungGyu <myunggyue.lee@osstem.com>
2022-01-13 17:05:09 +0900

    라디오 수정

--- src/components/common/OwRadio.vue
+++ src/components/common/OwRadio.vue
@@ -1,18 +1,23 @@
 <template>
-  <template v-for="({ name, value, disabled = false }, index) in items" :key="value">
-    <slot></slot>
-    <div class="ow-radio">
-      <input
-        type="radio"
-        :id="`${unique}-${index}`"
-        :name="unique"
-        :value="value"
-        :disabled="disabled"
-        v-model="checkedValue"
-      />
-      <label :for="`${unique}-${index}`">{{ name }}</label>
-    </div>
+  <template v-if="label">
+    <label class="radio-label">{{ label }}</label>
   </template>
+  <div class="radio-group">
+    <template v-for="({ name, value, disabled = false }, index) in items" :key="value">
+      <slot></slot>
+      <div class="ow-radio">
+        <input
+          type="radio"
+          :id="`${unique}-${index}`"
+          :name="unique"
+          :value="value"
+          :disabled="disabled"
+          v-model="checkedValue"
+        />
+        <label :for="`${unique}-${index}`">{{ name }}</label>
+      </div>
+    </template>
+  </div>
 </template>
 <script>
 import { computed } from 'vue';
@@ -20,6 +25,7 @@ import { expando } from '@/utils';
 export default {
   name: 'OwRadio',
   props: {
+    label: String,
     unique: {
       type: String,
       default: () => {
@@ -46,4 +52,13 @@ export default {
   },
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.radio-label {
+  font-size: 13px;
+  font-weight: 700;
+  letter-spacing: -1.08px;
+  color: #333;
+  margin-right: 6px;
+  flex-shrink: 0;
+}
+</style>
