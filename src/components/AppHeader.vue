4cd610c4b75b6d2800bf59283544916e741a52bc
kst <kst@osstem.com>
2022-01-20 08:56:11 +0900

    토큰 ttl 테스트

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -35,7 +35,7 @@
             알림<span class="count">{{ badgeCount }}</span>
           </button>
           <button type="button" class="setting">설정</button>
-          <button type="button" class="logout">로그아웃</button>
+          <button type="button" class="logout" @click="getTtl">로그아웃</button>
         </div>
       </div>
     </div>
@@ -43,7 +43,8 @@
 </template>
 <script>
 import store from '@/store';
-
+import login from '@/api/login.js';
+import router from '@/routes'; 
 import { computed, onMounted } from 'vue';
 import { useStore } from 'vuex';
 
@@ -76,6 +77,28 @@ export default {
     };
   },
 
-  methods: {},
+  methods: {
+
+    async getTtl() {
+      const now = new Date();
+
+      alert(store.state.login.ttl+"\n" + now.getTime());
+      return await this.getUserInfo();
+      
+      
+    },
+    async getUserInfo() {
+      const userData = await login.getUserInfo('/com/Auth/UserInfo', "25052408");
+      if (userData.data.data !== null) {
+        const userInfo = userData.data.data;
+        console.log(userInfo);
+        //localStorage.setItem("userInfo", JSON.stringify(userInfo));
+        //this.$store.commit('setUserInfo', userInfo);
+        //return await this.getMenus();
+        //this.$router.push("/");
+      }
+    },
+
+  },
 };
 </script>
