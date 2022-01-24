fe24af3e8a4246ba17089fff2791d58e0d2090c3
MyungGyu <myunggyue.lee@osstem.com>
2022-01-24 10:28:39 +0900

    article 100%

--- src/components/AppArticle.vue
+++ src/components/AppArticle.vue
@@ -1,6 +1,6 @@
 <template>
   <article class="ow-article">
-    <ow-flex-wrap class="size-full" col>
+    <ow-flex-wrap col>
       <slot></slot>
     </ow-flex-wrap>
   </article>
@@ -10,4 +10,8 @@ export default {
   name: 'AppArticle',
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.ow-article {
+  height: 100%;
+}
+</style>
