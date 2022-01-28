8c06e1e30dd471c9376e7ac221720421a922db35
MyungGyu <myunggyue.lee@osstem.com>
2022-01-28 13:57:34 +0900

    store의 module loader 추가

--- src/store/index.js
+++ src/store/index.js
@@ -7,48 +7,57 @@ import loading from './modules/loading';
 import comData from './modules/comData';
 
 const resetState = () => {
-    return {
-      login: {
-        state: {
-          accessToken: '',
-          userInfo: '',
-          ttl: '',
-          menus: [],
-        },
+  return {
+    login: {
+      state: {
+        accessToken: '',
+        userInfo: '',
+        ttl: '',
+        menus: [],
       },
-    };
+    },
+  };
+};
+
+const plugins = [createPersistedState()];
+
+const state = () => ({});
+
+const mutations = {
+  reset(state) {
+    Object.assign(state, resetState());
+  },
+};
+
+const getters = {};
+
+const modules = {
+  login,
+  menu,
+  notification,
+  loading,
+  comData,
+};
+
+function loadModules() {
+  const context = require.context('@/app', true, /(\/store\/).*\.js$/);
+  for (const key of context.keys()) {
+    const namespace = key
+      .match(/([A-Za-z0-9-_]+)\./)
+      .filter((locale) => !locale.endsWith('.'))
+      .at(0);
+    const { default: module } = context(key);
+    module.namespaced = true;
+    modules[namespace] = module;
+  }
 }
 
+loadModules();
+
 export default createStore({
-    plugins: [
-        /*
-                    createPersistedState({
-                        key: "user",
-                        paths: ["login"],
-                        filter: mutation => {
-                            if (mutation.type === "login/setUserInfo") {
-                                return true;
-                            }
-                            return false;
-                        },
-                        storage: window.sessionStorage
-                    })*/
-        createPersistedState(),
-    ],
-    state: {},
-    getters: {},
-    mutations: {
-        
-        reset(state) {
-           Object.assign(state, resetState())
-        }
-        
-    },
-    modules: {
-        login,
-        menu,
-        notification,
-        loading,
-        comData,
-    },
+  plugins,
+  state,
+  getters,
+  mutations,
+  modules,
 });
