01cf5e2f291e483a59d81c6608a3a7efca4f1e90
MyungGyu <myunggyue.lee@osstem.com>
2022-03-16 14:49:24 +0900

    사용자 정보가 포함된 데이터를 반환하는 메서드 추가

--- src/api/tree.js
+++ src/api/tree.js
@@ -17,3 +17,13 @@ export async function getOrganization(orgCd = null, withUsers = false) {
   const res = await http.get(URI, { orgCd });
   return res.data.data;
 }
+
+/**
+ * 조직도 정보 반환(사용자 포함)
+ *
+ * @param {String} orgCd 트리의 시작 조직 코드
+ * @returns {Array} 조직도 정보(사용자 포함)
+ */
+export function getOrganizationWithUsers(orgCd) {
+  return getOrganization(orgCd, true);
+}
