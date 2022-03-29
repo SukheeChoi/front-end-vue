eed8508d37ce92a006d56276bf92672e1cee2801
Jung Jaewon <jjwon@osstem.com>
2022-03-29 19:15:29 +0900

    comCode 수정

--- src/api/comCode.js
+++ src/api/comCode.js
@@ -2,7 +2,6 @@ import _ from 'lodash';
 import { OwMap } from '@/api/owMap.js';
 import restApi from '@/api/restApi.js';
 import CodeData from '@/store/modules/comData';
-import { CollectionView } from '@grapecity/wijmo';
 
 const url = '/com/Code';
 
@@ -21,21 +20,14 @@ export class ComCode {
     ComCode.loadList(reqList);
   }
 
-  static async get(code, displayFormat = '{name}') {
-    let itemSource = await ComCode.getValue(code, displayFormat);
-    console.log('1', code, itemSource);
-
-    if (itemSource.length <= 0) {
-      await ComCode.loadList(code);
+  static get(code, displayFormat = '{value} - {name}') {
+    if (code == 'USE_YN') {
+      displayFormat = null;
     }
-    console.log('2', code, ComCode._store[code]);
 
-    if (displayFormat) {
-      await ComCode.reformat(ComCode._store[code], displayFormat);
-    }
-    console.log('3', code, ComCode._store[code]);
+    let itemSource = ComCode._store[code];
 
-    return new CollectionView(ComCode._store[code]);
+    return itemSource;
   }
 
   static getMap(
@@ -45,53 +37,21 @@ export class ComCode {
     selectedValuePath = 'value',
     displayMemberPath = 'name'
   ) {
-    // let itemSource = ComCode.getValue(code, displayFormat);
-    // console.log('1', code, itemSource);
-
-    // if (itemSource.length <= 0) {
-    //   ComCode.loadList(code);
-    // }
-    // console.log('2', code, ComCode._store[code]);
-
-    // if (displayFormat) {
-    //   ComCode.reformat(ComCode._store[code], displayFormat);
-    // }
-    // console.log('3', code, ComCode._store[code]);
-
-    // return ComCode._store[code];
-
     return new OwMap(ComCode.getValue(code, displayFormat), filterKey, selectedValuePath, displayMemberPath);
   }
 
   static getValue(code, displayFormat = null) {
     let itemSource = ComCode._store[code];
 
-    // if (itemSource == null) {
-    //   ComCode.loadList(code);
-    //   itemSource = ComCode._store[code];
-    // }
-
     if (itemSource == null) {
       itemSource = [];
     }
-    // else {
-    //   if (displayFormat) {
-    //     itemSource = ComCode.reformat(itemSource, displayFormat);
-    //   }
-    // }
 
     return itemSource;
   }
 
-  //ComCode.loadList('OWTASK_CD,USER_STATCD');
   static async loadList(reqList, id = '') {
-    console.log('reqList', reqList);
-    // if (reqList.length == 0) {
-    //   return;
-    // }
-
     let resData = await restApi.getList(url, { codeList: reqList }, id);
-    console.log('resData', resData);
 
     if (resData.data.data) {
       let codes = reqList.split(',');
