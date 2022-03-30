da1206a6adae9408dd2e046e291f26ddd8baea08
OST25070511N\osstem <online9@osstem.com>
2022-03-30 13:58:41 +0900

    deepClione 적용

--- src/api/comCode.js
+++ src/api/comCode.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import { OwMap } from '@/api/owMap.js';
 import restApi from '@/api/restApi.js';
 import CodeData from '@/store/modules/comData';
+import { CollectionView } from '@grapecity/wijmo';
 
 const url = '/com/Code';
 
@@ -9,27 +10,43 @@ export class ComCode {
   static _store = CodeData;
 
   static get(code, format = '{value} - {name}') {
+    code = code.replace('link:', '').trim();
+    let itemSource = ComCode._store[code];
+    let newSource = [];
+
+    if (itemSource.length > 0) {
+      newSource = _.cloneDeep(itemSource);
+    }
+
+    if (newSource.length > 0 && code !== 'USE_YN') {
+      newSource = ComCode.reformat(newSource, format);
+    }
+
+    return new CollectionView(newSource);
+  }
+
+  static getMap(code, filter = null, format = '{name}', selectedValuePath = 'value', displayMemberPath = 'name') {
     let itemSource = ComCode._store[code];
 
     if (itemSource.length > 0 && code !== 'USE_YN') {
-      itemSource = ComCode.reforamt(itemSource, format);
+      itemSource = ComCode.reformat(itemSource, format);
     }
 
-    return itemSource;
-  }
-
-  static getMap(code, filter = null, format = '{name}', value = 'value', name = 'name') {
-    return new OwMap(ComCode.get(code, format), filter, value, name);
+    return new OwMap(itemSource, filter, selectedValuePath, displayMemberPath);
   }
 
   static async loadList(codeList, id = '') {
     let response = await restApi.getList(url, { codeList: codeList }, id);
     let resData = response.data.data;
-    let codes = codeList.split(',');
+    let codes = codeList.replace('link:', '').split(',');
 
     if (resData) {
+      console.log("resData ", resData);
+
       codes.forEach((code) => {
-        var codeData = resData[code];
+        code = code.trim();
+        console.log("code ", code);
+        var codeData = resData[code.trim()];
 
         if (codeData.length == 0) {
             codeData = [];
@@ -39,14 +56,13 @@ export class ComCode {
       });
     } else {
       codes.forEach((code) => {
+        code = code.trim();
         Object.assign(ComCode._store, { [code]: [] });
       });
     }
   }
 
-  static reformat(itemSource, displayFormat) {
-    let reformatSource = _.cloneDeep(itemSource);
-
+  static reformat(reformatSource, displayFormat) {
     for (var idx in reformatSource) {
       reformatSource[idx].name = displayFormat
         .replace('{value}', reformatSource[idx].value)
