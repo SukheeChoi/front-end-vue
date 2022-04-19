bc06f02b9c3f9a749bc963fdf43ddb362a620948
MyungGyu <myunggyue.lee@osstem.com>
2022-04-19 10:31:17 +0900

    sock 추가

---
+++ src/plugins/sock.js
@@ -0,0 +1,72 @@
+'use strict';
+
+import SockJS from 'sockjs-client';
+import { Client } from '@stomp/stompjs';
+
+// PUBLISHES
+function publish(client, message) {
+  client.publish({
+    destination: '/app/pub',
+    body: JSON.stringify(message),
+  });
+}
+
+// SUBSCRIBES
+function subscribe(store, message) {
+  console.log('message', store, message);
+  // store.receiveList.push();
+  const item = JSON.parse(message.body);
+
+  const now = new Date();
+  let dateTime =
+      _.padStart(now.getMonth() + 1, 2, 0) +
+      '-' +
+      _.padStart(now.getDate(), 2, 0) +
+      ' ' +
+      _.padStart(now.getHours(), 2, 0) +
+      ':' +
+      _.padStart(now.getMinutes(), 2, 0),
+    index = now.getMonth() || now.getDate() || now.getHours() || now.getMinutes() || now.getMilliseconds();
+
+  store.commit('addMessage', {
+    open: true,
+    index,
+    userNm: '홍길동',
+    orgNm: 'OW공통개발팀',
+    dateTime,
+    message: item.message,
+  });
+  store.commit('setOpenAlert');
+  store.commit('setBadgeCount', state.receiveList.length);
+}
+
+export default {
+  install: (app, options) => {
+    const store = app.config.globalProperties.$store;
+    const client = new Client({
+      webSocketFactory: () => {
+        return new SockJS('http://10.160.39.3:8010/ws');
+      },
+      onConnect: (frame) => {
+        console.log('connect', frame);
+        client.subscribe('/topic/messages', (message) => {
+          subscribe(store, message);
+        });
+      },
+      onStompError: (frame) => {
+        console.log('stomp error', frame);
+      },
+    });
+    client.activate();
+
+    console.log('client', app, client);
+
+    app.mixin({
+      methods: {
+        $publish: function (message) {
+          publish(client, message);
+        },
+      },
+    });
+  },
+};
