17d6ede2b3d83791232fa2e8f1f83342c073cb52
MyungGyu <myunggyue.lee@osstem.com>
2022-04-20 09:13:32 +0900

    조직도 변경

--- src/components/wijmo/tree/OwOrgTreeView.vue
+++ src/components/wijmo/tree/OwOrgTreeView.vue
@@ -1,26 +1,32 @@
 <template>
-  <ow-tree-view
-    :initialized="initTreeView"
-    :items-source="treeData"
-    :format-item="onFormatItem"
-    :display-member-path="['orgNm', 'userNm']"
-    child-items-path="children"
-    v-bind="$attrs"
-  ></ow-tree-view>
+  <ow-flex-wrap>
+    <ow-flex-item class="size-full" col>
+      <ow-input search @lookup="lookup"></ow-input>
+      <ow-tree-view
+        :initialized="initTreeView"
+        :items-source="treeData"
+        :format-item="formatItem"
+        :display-member-path="['orgNm', 'userNm']"
+        child-items-path="children"
+        :expand-on-load="false"
+        v-bind="$attrs"
+      ></ow-tree-view>
+    </ow-flex-item>
+  </ow-flex-wrap>
 </template>
 <script>
 import _ from 'lodash';
 
-import { computed, reactive, toRef, toRefs } from 'vue';
-
-import { addClass, removeClass, hasClass } from '@grapecity/wijmo';
-
+import { reactive, toRefs } from 'vue';
+import { addClass, removeClass } from '@grapecity/wijmo';
 import OwTreeView from '@/components/wijmo/tree/OwTreeView';
-
 import { getOrganization } from '@/api/tree';
 
-const CLS_F = 'filtered';
-const CLS_M = 'matched';
+const CLS_FILTERED = 'filtered';
+const CLS_MATCHED = 'matched';
+
+const CLS_ORG = 'org';
+const CLS_USER = 'user';
 
 export default {
   inheritAttrs: false,
@@ -38,27 +44,77 @@ export default {
       treeData: [],
     });
 
+    const traversal = (nodes, keyword) => {
+      const regex = /(<span class="highlight">|<\/span>)/gi;
+      const keywordReg = new RegExp(keyword, 'gi');
+      for (const node of nodes) {
+        const dataItem = node.dataItem;
+        const name = dataItem.orgNm ?? dataItem.userNm ?? '';
+        const isMatched = !!keyword && name.includes(keyword);
+        if (isMatched) {
+          for (let self = node; self; self = self.parentNode) {
+            self.isCollapsed = false;
+            const element = self.element;
+            const html = element.innerHTML.replace(regex, '');
+            element.innerHTML = html.replace(keywordReg, '<span class="highlight">$&</span>');
+            addClass(element, CLS_MATCHED);
+          }
+        } else {
+          node.isCollapsed = !!node.parentNode;
+          const element = node.element;
+          const html = element.innerHTML.replace(regex, '');
+          element.innerHTML = html;
+          removeClass(element, CLS_MATCHED);
+        }
+        if (node.nodes) {
+          traversal(node.nodes, keyword);
+        }
+      }
+    };
+
+    const lookup = (keyword) => {
+      const treeView = state.treeView;
+      keyword = keyword.replace(/\s/g, '');
+      if (!keyword) {
+        removeClass(treeView.hostElement, CLS_FILTERED);
+      }
+      traversal(treeView.nodes, _.toUpper(keyword));
+      if (keyword) {
+        addClass(treeView.hostElement, CLS_FILTERED);
+      }
+    };
+
     const initTreeView = async (s) => {
       state.treeView = s;
       state.treeData = await getOrganization(props.orgCd, props.withUsers);
+
+      s.loadedItems.addHandler(() => {
+        const root = s.nodes.at(0);
+        if (root) {
+          root.isCollapsed = false;
+        }
+      });
+
       if (props.initialized) {
         props.initialized(s);
       }
     };
 
-    const onFormatItem = (s, e) => {
-      if (e.dataItem.orgNm) {
-        e.element.classList.add('org');
-      }
-      if (e.dataItem.empNo) {
-        e.element.classList.add('user');
+    const formatItem = (s, e) => {
+      const item = e.dataItem;
+      const element = e.element;
+      if (item.orgNm) {
+        addClass(element, CLS_ORG);
+      } else if (item.userNm) {
+        addClass(element, CLS_USER);
       }
     };
 
     return {
       ...toRefs(state),
       initTreeView,
-      onFormatItem,
+      formatItem,
+      lookup,
     };
   },
 };
@@ -81,26 +137,14 @@ $user-img: '~@/assets/images/icon/ico_people.svg';
   }
 }
 .wj-treeview {
-  position: relative;
-  &.filtering::after {
-    content: '찾는중...';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: flex-start;
-    padding-top: 20%;
-    background-color: rgba(255, 255, 255, 1);
-  }
   &.filtered {
     :deep(.wj-node:not(.matched)) {
       display: none;
     }
+    :deep(.highlight) {
+      background-color: yellow;
+    }
   }
-
   :deep(.org) {
     @include image-default(#{$org-img});
   }
