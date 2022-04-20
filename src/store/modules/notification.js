b39ca592aea215f141bbe6464c8e518cba4ed21e
MyungGyu <myunggyue.lee@osstem.com>
2022-04-20 13:44:29 +0900

    알림 임시 수정

--- src/store/modules/notification.js
+++ src/store/modules/notification.js
@@ -1,78 +1,74 @@
+'use strict';
+
+import { getAllUnconfirmedMessages, confirmMessage, allConfirmMessages } from '@/api/message';
+
+// 보여줄 최대 메시지 개수
+const NUMBER_OF_MESSAGES_TO_DISPLAY = process.env.VUE_APP_NUMBER_OF_MESSAGES_TO_DISPLAY ?? 20;
+
 const state = () => ({
-  // AppHeader
-  badge: {
-    count: 0,
-  },
-  // AppAside
-  sidebar: {
-    open: false,
-  },
-  alert: {
-    open: false,
-    index: 0,
-    dateTime: '',
-    userNm: '',
-    orgNm: '',
-    message: '',
-  },
-  receiveList: [],
+  messages: [],
 });
 
 const mutations = {
-  setBadgeCount(state, value) {
-    state.badge.count = value;
+  // 메시지 초기화
+  setMessages(state, messages) {
+    if (state.messages.length > 0) {
+      console.warn('메시지가 안비어있음');
+      return;
+    }
+    state.messages = messages;
   },
-  setOpenSidebar(state) {
-    state.sidebar.open = true;
+  // 메시지 추가
+  addMessage(state, message) {
+    state.messages.push(message);
   },
-  setCloseSidebar(state) {
-    state.sidebar.open = false;
+  // 메시지 지우기
+  removeMessage(state, message) {
+    const at = state.messages.indexOf(message);
+    if (at > -1) {
+      state.messages.splice(at, 1);
+    }
   },
-  setOpenAlert(state) {
-    state.alert.open = true;
-    setTimeout(() => {
-      state.alert.open = false;
-    }, 3000);
-  },
-  setCloseAlert(state) {
-    state.alert.open = false;
-  },
-  receiveMessage(state, msg) {
-    state.alert = msg;
-    state.receiveList.unshift(msg);
-  },
-  removeMessage(state, msg) {
-    const getIdx = (element) => element.index === msg.index;
-    state.receiveList.splice(state.receiveList.findIndex(getIdx), 1);
-  },
-  removeAllMessage(state) {
-    state.receiveList.splice(0, state.receiveList.length);
+  // 메시지 비우기
+  clearMessages(state) {
+    state.messages = [];
   },
 };
 
-const actions = {};
+const actions = {
+  // 메시지 초기화
+  async initMessages({ commit }) {
+    // 서버로 확인하지 않은 메시지 목록을 받아서 설정
+    commit('setMessages', await getAllUnconfirmedMessages()); // 두 번째 인자는 await 서버 다녀오기
+  },
+  // 메시지 확인(지우기)
+  async confirmMessage({ commit }, message) {
+    // 서버로 메시지 확인에 대한 처리 이후에 메시지 지우기
+    if (await confirmMessage(message.id)) {
+      commit('removeMessage', message);
+    }
+  },
+  // 모든 메시지 확인(지우기)
+  async allConfirmMessages({ commit }) {
+    // 서버로 메시지 확인에 대한 처리 이후에 메시지 초기화
+    if (await allConfirmMessages()) {
+      commit('clearMessages');
+    }
+  },
+};
 
 const getters = {
-  getBadgeCount(state) {
-    return state.receiveList.length;
+  // 알림 개수
+  badgeCount(state) {
+    return state.messages.length;
   },
-  getAlertUserName(state) {
-    return state.alert.userNm;
+  // 최근 메시지
+  latestMessage(state) {
+    return state.messages.at(-1);
   },
-  getAlertOrgName(state) {
-    return state.alert.orgNm;
-  },
-  getAlertDateTime(state) {
-    return state.alert.dateTime;
-  },
-  getAlertMessage(state) {
-    return state.alert.message;
-  },
-  getAlertIndex(state) {
-    return state.alert.index++;
-  },
-  getAlertOpen(state) {
-    return state.alert.open;
+  // 최근 메시지 목록
+  latestMessages(state) {
+    return state.messages.slice(-1 * NUMBER_OF_MESSAGES_TO_DISPLAY);
   },
 };
 
